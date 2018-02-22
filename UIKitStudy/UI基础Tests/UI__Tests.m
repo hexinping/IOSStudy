e1dfb398f7001349805301a22a61e325f90149e5
hexinping <hexinping456@126.com>
2018-02-22 08:02:29 +0800

    IOS开发UI基础工程添加

---
+++ UIKitStudy/UI基础Tests/UI__Tests.m
@@ -0,0 +1,39 @@
+//
+//  UI__Tests.m
+//  UI基础Tests
+//
+//  Created by hexinping on 2018/2/22.
+//  Copyright © 2018年 hexinping. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UI__Tests : XCTestCase
+
+@end
+
+@implementation UI__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
